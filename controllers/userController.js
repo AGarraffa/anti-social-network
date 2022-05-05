21c9365e32c0148fe3afa06967d41a231e4efe48
Alfred Garraffa <agarraffa@gmail.com>
2022-05-04 18:23:20 -0600

    finished writing userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,31 +1,7 @@
-// /api/users
-
-// GET all users
-
-// GET a single user by its _id and populated thought and friend data
-
-// POST a new user:
-
-// // example data
-// {
-//   "username": "lernantino",
-//   "email": "lernantino@gmail.com"
-// }
-// PUT to update a user by its _id
-
-// DELETE to remove user by its _id
-
-// BONUS: Remove a user's associated thoughts when deleted.
-
-// /api/users/:userId/friends/:friendId
-
-// POST to add a new friend to a user's friend list
-
-// DELETE to remove a friend from a user's friend list
-
 const { ObjectId } = require('mongoose').Types;
 const { User, Thought } = require('../models');
 
+// gets all users
 async function getUsers(req, res) {
     
     try {
@@ -34,18 +10,18 @@ async function getUsers(req, res) {
         return res.status(200).json(userData);
 
     } catch (err) {
-
         console.log(err);
         return res.status(500).json(err)
-
     }
 };
 
+
+// finds a single user
 async function getSingleUser(req, res) {
 
     try {
         
-        const userData = await User.findOne({ _id: req.params.userId }).select('-__v');
+        const userData = await User.findById(req.params.userId).select('-__v');
     
         if (!userData) {
             return res.status(404).json({ message: 'No user with that ID found' });
@@ -55,13 +31,13 @@ async function getSingleUser(req, res) {
 
 
     } catch (err) {
-
         console.log(err);
         return res.status(500).json(err);
-    
     }
 };
 
+
+// adds a user
 async function addUser(req, res) {
 
     try {
@@ -70,13 +46,116 @@ async function addUser(req, res) {
         return res.status(200).json(userData);
     
     } catch (err) {
-
         console.log(err);
         return res.status(500).json(err);
-    
     }
 
 };
 
 // TODO: write the put and delete route, as well as work on deleting thoughts when the user is deleted and then write post and delete to the friends list
 
+// deletes a user
+async function deleteUser(req, res) {
+
+    try {
+
+        const userData = await User.findByIdAndDelete(req.params.userId).select('-__v');
+
+        if (!userData) {
+            return res.status(404).json({message: 'No user with that ID found'});
+        }
+
+        const thoughtData = await cascadeThought(req, res);
+
+        return res.status(200).json(userData);
+
+        // 
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+};
+
+
+// used for deleting thoughts of a removed user
+async function cascadeThought(req, res) {
+
+    try {
+
+        const thoughtData = await Thought.deleteMany({userId: req.params.userId}).select('-__v');
+
+        return res.status(200).json(thoughtData);
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+};
+
+
+// updates a single user
+async function updateUser(req, res) {
+
+    let update = {};
+
+    // logic to only update the fields that are provided
+    if (req.body.userName) {
+        update.userName = req.body.userName;
+    };
+
+    if (req.body.email) {
+        update.email = req.body.email;
+    };
+
+    try {
+
+        const userData = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { update }
+        ).select('-__v');
+
+
+        if (!userData) {
+            return res.status(404).json({ message: 'No user with that ID found' });
+        }
+
+        return res.status(200).json(userData);
+
+        // just messing around with ternary operators
+        // !userData ? res.status(404).json({message: 'No user with that ID found'}) : res.status(200).json(userData);
+        
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err)
+    }
+};
+
+
+async function addFriend(req, res) {
+
+    try {
+
+        const userData = await User.findById(req.params.userId).select('-__v');;
+
+        if (!userData) {
+            return res.status(404).json({ message: 'No user with that ID found'});
+        }
+
+        let friendArr = userData.friends;
+
+        friendArr.push(req.params.friendId);
+
+        const updatedUserData = await User.findOneAndUpdate(
+            {_id: req.params.userId},
+            { friends: friendArr}
+            ).select('-__v');
+
+        return res.status(200).json(updatedUserData);
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+}
+
+module.exports = { getUsers, getSingleUser, addUser, deleteUser, updateUser, addFriend };
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser, 
+    addUser, 
+    deleteUser, 
+    updateUser, 
+    addFriend
+} = require('../../controllers/userController');
+
+// api/users/ end route
+
+router.route('/')
+    .get(getUsers)
+    .post(addUser);
+
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser)
+
+router.route('/:userId/friend/:friendId').put(addFriend);
